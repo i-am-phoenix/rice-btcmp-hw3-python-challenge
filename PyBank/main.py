495498d90a2790d82ceb8e5f6ab3b793d7fedd17
troubleUA <iryna.p.marchiano@gmail.com>
2021-04-30 16:45:13 -0500

    PyBank: added code to calculate number of months of data available for analysis

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,24 +1,23 @@
 import os
 import csv
 
-
 in_folder="Resources"
 in_file="budget_data.csv"
 
-
 csvpath=os.path.join(in_folder,in_file)
-
-print(csvpath)
+#print(csvpath)
 
 # Initialize row count
 nrows=0
 net_total=0
 with open(csvpath) as csvfile:
     csvreader=csv.reader(csvfile,delimiter=",")
-    print(csvreader)
+    #print(csvreader)
 
     csv_header=next(csvreader)
-    print(f"CSV file header: {csv_header}")
+    #print(f"CSV file header: {csv_header}")
 
     for row in csvreader:
-        print(row)
+        nrows=nrows+1
+
+print(nrows)
