94534953738d174a809f828fe36361ac71c3d958
i-am-phoenix <iryna.p.marchiano@gmail.com>
2021-05-02 00:18:34 -0500

    PyParagraph: Calculating average letter count

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -9,15 +9,28 @@ txt_file_path=os.path.join(folder,file1)
 
 with open(txt_file_path,'r') as txt_file:
     txt_sentences=[row for row in csv.reader(txt_file,delimiter='.')]
-print(f"{len(txt_sentences[0])}\t Sentences")
 
 with open(txt_file_path,'r') as txt_file:
     txt_words=[row for row in csv.reader(txt_file,delimiter=' ')]
-print(f"{len(txt_words[0])}\t Words")
 
-for sentence in txt_sentences[0]:
-    print(sentence)
-
+# txt_words
+words=[]
 for word in txt_words[0]:
-    print(word)
-    
+    words.append(word)
+
+print(words)
+
+letter_sum=0
+counter=0
+for letter in words:
+    # print(len(letters[counter]))
+    letter_sum+=len(words[counter])
+    counter+=1
+
+letters_per_word=letter_sum/len(txt_words[0])
+
+print(f"Paragraph Analysis\n-----------------\n")
+print(f"Approximate Word Count: {len(txt_words[0])}")
+print(f"Approximate Sentence Count: {len(txt_sentences[0])}")
+print(f"Average Letter Count: {round(letters_per_word,2)}")
+print(f"Average Sentence Length: 24.0")
