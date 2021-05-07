ee2c7815d2ad8dc6cb95b79df5e3efe9ddda79ed
i-am-phoenix <iryna.p.marchiano@gmail.com>
2021-05-07 10:48:52 -0500

    updating README.md

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import re
 
 folder="Resources"
 file1="paragraph_1.txt"
@@ -7,30 +8,59 @@ file2="paragraph_2.txt"
 
 txt_file_path=os.path.join(folder,file1)
 
-with open(txt_file_path,'r') as txt_file:
-    txt_sentences=[row for row in csv.reader(txt_file,delimiter='.')]
+# with open(txt_file_path,'r') as txt_file:
+#     txt_sentances=[row for row in csv.reader(txt_file,delimiter='.')]
+
+# sentances=[]
+# for sentance in txt_sentances[0]:
+#     sentances.append(sentance)
+
+# print(sentances[1])
+
+# word_sum=0
+# counter=0
+# for word in sentances:
+#     # print(len(letters[counter]))
+#     word_sum+=len(sentances[counter])
+#     print(word_sum)
+#     counter+=1
+
+# words_per_sentence=word_sum/len(txt_sentances[0])
+
+# with open(txt_file_path,'r') as txt_file:
+#     txt_words=[row for row in csv.reader(txt_file,delimiter=' ')]
+
+# # txt_words
+# words=[]
+# for word in txt_words[0]:
+#     words.append(word)
+
+# # print(words)
+
+# letter_sum=0
+# counter=0
+# for letter in words:
+#     # print(len(letters[counter]))
+#     letter_sum+=len(words[counter])
+#     counter+=1
+
+# letters_per_word=letter_sum/len(txt_words[0])
+nmb_words=[]
+
 
 with open(txt_file_path,'r') as txt_file:
-    txt_words=[row for row in csv.reader(txt_file,delimiter=' ')]
-
-# txt_words
-words=[]
-for word in txt_words[0]:
-    words.append(word)
-
-print(words)
-
-letter_sum=0
-counter=0
-for letter in words:
-    # print(len(letters[counter]))
-    letter_sum+=len(words[counter])
-    counter+=1
-
-letters_per_word=letter_sum/len(txt_words[0])
-
-print(f"Paragraph Analysis\n-----------------\n")
-print(f"Approximate Word Count: {len(txt_words[0])}")
-print(f"Approximate Sentence Count: {len(txt_sentences[0])}")
-print(f"Average Letter Count: {round(letters_per_word,2)}")
-print(f"Average Sentence Length: 24.0")
+    par=txt_file.read()
+    sentences=par.split(".")
+    print(f"{len(sentences)}\n")
+    for sent in sentences:
+        words=sent.split(" ")
+        print(f"{len(words)}")
+        nmb_words.append(len(words))
+        # print(nmb_words)
+avg_nmb_words=sum(nmb_words)/len(nmb_words)
+print(avg_nmb_words)
+# print(f"Paragraph Analysis\n-----------------\n")
+# print(f"Approximate Word Count: {len(txt_words[0])}")
+# print(f"Approximate Sentence Count: {len(txt_sentances[0])}")
+# print(f"Average Letter Count: {round(letters_per_word,2)}")
+# print(f"Average Sentence Length: {words_per_sentence}")
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# rice-btcmp-hw3-python-challeng
-Use Python to solve 2 problems: 1) analyze financial records of a company; 2) modernize a vote counting process of a small rural town
+# rice-btcmp-hw3-python-challenge
+Current repository contains two projects where Python default capabilities were being utilized.
+
+## PyBank: Analysis of financial records of a company; 
+
+### Input data: 
+
+Input data consisted of a <b>*.csv</b>["/PyBank/Resources/budget_data.csv"]("primary input data") file which contained the following data:
+
+Analysis and output:
+
+## PyPoll: Modernization of a vote counting process of a small rural town
+
+
+
